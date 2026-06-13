c7525214d01176406c13bb868fa45d8ec9c8f90f
PineappleBirthdayCake <PineappleBirthdayCake@gmail.com>
2026-06-13 18:47:57 -0300

    root de reactrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App";
+import Notes from "./pages/Notes";
+import Home from "./pages/Home";
+import Navegacion from "./components/Navegacion";
+
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <Navegacion />
+    <Routes>
+       <Route path="/" element={<Home />} />
+      <Route path="/notes" element={<Notes />} />
+    </Routes>
+  </BrowserRouter>,
+);
+
