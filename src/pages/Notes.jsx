2e9feb781ff90e8cfa72e0c29034708cddffd1c3
PineappleBirthdayCake <PineappleBirthdayCake@gmail.com>
2026-06-13 18:43:36 -0300

    Pagina de notas + css

---
+++ src/pages/Notes.jsx
@@ -0,0 +1,12 @@
+import Bottombar from "../components/Bottombar"
+import NotesBody from "../components/NotesBody"
+import './Notes.css';
+
+export default function Home() {
+    return (
+        <>
+        <NotesBody />
+        <Bottombar />
+        </>
+    )
+}
